0e80ee72b6d047417ea66f9e3def5558e098813d
martijn <tijntje_7@msn.com>
2014-09-28 22:45:27 +0200

    Added warning before leaving unsaved changes, and added a very slight background shade

--- client/client.js
+++ client/client.js
@@ -3,3 +3,13 @@ Meteor.startup(function () {
     Session.setDefault("editingNote", "");
     Session.setDefault("newNoteNotify", false);
 });
+
+Tracker.autorun(function () {
+    if (Session.get("content") || Session.get("editingNote")) {
+        window.onbeforeunload = function () {
+            return "You haven't finished writing your note, are you sure you want to leave?";
+        };
+    } else {
+        window.onbeforeunload = Function.prototype;
+    }
+});
