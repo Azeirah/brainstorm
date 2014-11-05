1df7221a9f557e2c8e442cef24eef9cbaa6fa63a
martijn <tijntje_7@msn.com>
2014-11-06 00:17:30 +0100

    You can now add a new board

--- client/board/boardNavigation.js
+++ client/board/boardNavigation.js
@@ -3,3 +3,14 @@ Template.boardNavigation.helpers({
         return Boards.find({});
     }
 });
+
+Template.boardNavigation.events({
+    "submit #newBoard": function (e, t) {
+        var $boardName = t.find("#boardName");
+        e.preventDefault();
+        e.stopPropagation();
+        Boards.insert({name: $boardName.value});
+        $boardName.value = "";
+        return false;
+    }
+});
