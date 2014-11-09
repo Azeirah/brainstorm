6808d20ec6b27dfe97fcf4c27d8b2fbff58839d6
martijn <tijntje_7@msn.com>
2014-11-09 13:38:31 +0100

    Added a warning before deleting a board

--- client/board/manageBoards.js
+++ client/board/manageBoards.js
@@ -1,6 +1,13 @@
 Template.manageBoards.helpers({
     boards: function () {
         return Boards.find({});
+    },
+    confirmBoardRemoval: function () {
+        return function (collection, id) {
+            var doc = Boards.findOne({_id: id});
+            if (window.confirm("Really remove " + doc.name + "? Your posts will not be removed, but there is currently no way to access them after deleting the board.")) {
+                this.remove();
+            };
+        }
     }
 });
-
