12c59f39ff4dcb2ea2b1cfd9372fa51fb6f50785
martijn <tijntje_7@msn.com>
2014-11-29 00:22:46 +0100

    Added emphasis to a deletion warning

--- client/board/manageBoards.js
+++ client/board/manageBoards.js
@@ -5,7 +5,7 @@ Template.manageBoards.helpers({
     confirmBoardRemoval: function () {
         return function (collection, id) {
             var doc = Boards.findOne({_id: id});
-            if (window.confirm("Really remove " + doc.name + "? Your posts will not be removed, but there is currently no way to access them after deleting the board.")) {
+            if (window.confirm("Really remove " + doc.name + "? Your posts won't be permanently removed, but there is currently NO WAY to access them after deleting the board.")) {
                 this.remove();
             }
         }
