a15e8a463ea6ab78ad8be344d42514c31ec9e65a
martijn <tijntje_7@msn.com>
2014-11-30 03:16:29 +0100

    Revert "Removed edit and delete button in the tag view"
    This reverts commit 23eed6023ba219465beb70a32bb3c306555cc2e5.

--- client/tags/tag.js
+++ client/tags/tag.js
@@ -1,9 +1,5 @@
 Template.tag.helpers({
     "tagNote": function (tag) {
-        var notes = Notes.find({tags: {$in: [tag]}}).fetch();
-        notes.forEach(function (note) {
-            note.tag = true;
-        });
-        return notes;
+      return Notes.find({tags: {$in: [tag]}}).fetch();
     }
 });
